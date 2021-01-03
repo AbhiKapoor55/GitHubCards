d8482138c5611c408d42ba879c3147b3d93bab78
AbhiKapoor55 <abhishek.kapoor@mail.utoronto.ca>
2021-01-02 21:56:37 -0500

    added CardList component

--- github-cards/src/App.js
+++ github-cards/src/App.js
@@ -1,16 +1,31 @@
 import './App.css';
 import React from 'react';
 
-// Card Component 
+const testData = [
+  {name: "Dan Abramov", avatar_url: "https://avatars0.githubusercontent.com/u/810438?v=4", company: "@facebook"},
+  {name: "Sophie Alpert", avatar_url: "https://avatars2.githubusercontent.com/u/6820?v=4", company: "Humu"},
+  {name: "Sebastian Markbåge", avatar_url: "https://avatars2.githubusercontent.com/u/63648?v=4", company: "Facebook"},
+];
 
+// CardList Component 
+
+const CardList = (props) => {
+  return (
+    <div>
+      {testData.map(profile => <Card profile={profile}/>)}
+    </div>
+  );
+}
+
+// Card Component 
 class Card extends React.Component {
   render() {
     return (
       <div className="github-profile">
-        <img src="https://placehold.it/75" alt=""/>
+        <img src={this.props.profile.avatar_url} alt=""/>
         <div className="info">
-          <div className="name">Name Here...</div>
-          <div className="company">Company Here...</div>
+          <div className="name">{this.props.profile.name}</div>
+          <div className="company">{this.props.profile.company}</div>
         </div>
       </div>
     );
@@ -22,7 +37,7 @@ class App extends React.Component {
     return (
       <React.Fragment>
         <div className="header">{this.props.title}</div>
-        <Card />
+        <CardList />
       </React.Fragment>
     );
   }
